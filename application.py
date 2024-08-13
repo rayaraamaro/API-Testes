7038a115763d514f887dc89fd998dc7e483df25c
Rayara Amaro Figueiredo <rayaraamaro@ibm.com>
2024-08-13 14:03:47 -0300

    Atualizando APIs

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 
@@ -19,7 +19,35 @@ def index():
 
 @app.route('/drinks')
 def get_drinks():
-    return "drinks"
+    drinks = Drink.query.all()
+
+    output = []
+    for drink in drinks:
+        drink_data = {'name': drink.name, 'description': drink.description}
+        output.append(drink_data)
+
+    return {"drinks": output}
+
+@app.route('/drinks/<id>')
+def get_drink(id):
+    drink = Drink.query.get_or_404(id)
+    return {"name": drink.name, "description": drink.description}
+
+@app.route('/drinks', methods=['POST'])
+def add_drink():
+    drink = Drink(name=request.json['name'], description=request.json['description'])
+    db.session.add(drink)
+    db.session.commit()
+    return {'id': drink.id}
+
+@app.route('/drinks.<id>', methods=['DELETE'])
+def delete_drink(id):
+    drink = Drink.query.get(id)
+    if drink is None:
+        return {"error": "not found"}
+    db.session.delete(drink)
+    db.session.commit()
+    return {"mensage": "yeet!@"}
 
 if __name__ == '__main__':
     app.run(debug=True)
